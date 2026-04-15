4ff727beb3bb2152d271a48d68652576dee82b49
tu0800982-rgb <tu0800982@gmail.com>
2026-04-15 16:31:20 +0700

    Create LastButtonModel.java

---
+++ LastButtonModel.java
@@ -0,0 +1,30 @@
+package model;
+
+public class LastButtonModel {
+private int value;
+
+public LastButtonModel() {
+	this.value = 1;
+}
+
+public int getValue() {
+	return value;
+}
+
+public void setValue(int value) {
+	this.value = value;
+}
+
+public void setValue_1() {
+	this.value = 1;
+}
+public void setValue_2() {
+	this.value = 2;
+}
+public void setValue_3() {
+	this.value = 3;
+}
+public void setValue_4() {
+	this.value = 4;
+}
+}
